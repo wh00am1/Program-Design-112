af1571374386f2d6843f3591698330c9bb578da8
wh00am1 <ethan.418.63@gmail.com>
2023-09-26 13:50:30 +0800

    update

--- Class-Ex/W4-format-IO.c
+++ Class-Ex/W4-format-IO.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main(){
-	int i=40;
-	float x=839.21f;
-	printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
+	int i=40, j;
+	float x=839.21f, y;
+	printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i); // demo code for first part of the slide
 	printf("|%10.3f|%10.3e|%-10e|\n", x, x, x);
+
+	scanf("%d%d%f%f", &i, &j, &x, &y); // demo code for second part of the slide
+	printf("%d %d %.3f %.3f", i, j, x, y);
 	return 0;
 }
